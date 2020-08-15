9922a4768f34a32aec726dd724db1d8c7b0a2f5d
torreseam <615694479+torreseam@users_noreply.github.com>
2020-08-14 23:29:54 -0500

    graphql users

---
+++ client/src/utils/mutations.js
@@ -0,0 +1,86 @@
+import gql from 'graphql-tag';
+
+// User login
+export const LOGIN_USER = gql`
+    mutation loginUser($email: String!, $password: String!) {
+        login(email: $email, password: $password) {
+        token
+            user {
+                _id
+                username
+                email
+                bookCount
+                savedBooks {
+                    bookId
+                    title
+                    description
+                    authors
+                    link
+                    image
+         }
+       }
+     }
+   }
+`;
+
+//  add user 
+export const ADD_USER = gql`
+    mutation addUser($username: String!, $email: String!, $password: String!) {
+        addUser(username: $username, email: $email, password: $password) {
+        token
+            user {
+                _id
+                username
+                email
+                bookCount
+                savedBooks {
+                    bookId
+                    title
+                    description
+                    authors
+                    image
+                    link
+                }
+            }
+        }
+    }
+`;
+
+// save book
+export const SAVE_BOOK = gql`
+    mutation saveBook($input: bookInput!) {
+        saveBook(input: $input) {
+            _id
+            username
+            email
+            savedBooks {
+                bookId
+                authors
+                image
+                description
+                title
+                link
+            }
+        }
+    }
+`;
+
+// remove books
+export const REMOVE_BOOK = gql`
+    mutation removeBook($bookId: String!) {
+        removeBook(bookId: $bookId) {
+            _id
+            username
+            email
+            bookCount
+            savedBooks {
+                bookId
+                authors
+                image
+                description
+                title
+                link
+            }
+        }
+    }
+`;
